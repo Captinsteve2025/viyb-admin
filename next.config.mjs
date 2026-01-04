257f06a250abbfce4c93c3112d2a1d3b98a848e0
Captinsteve2025 <steve@virginislandsyachtbroker.com>
2026-01-04 12:54:13 -0400

    Initial commit: VIYB Payload CMS admin panel

--- next.config.mjs
+++ next.config.mjs
@@ -2,14 +2,17 @@ import { withPayload } from '@payloadcms/next/withPayload'
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // Your Next.js config here
+  allowedDevOrigins: [
+    'https://3000-kimqdbctszrjryjdgvuvcoeyvopcbpnv.preview.same-app.com',
+    'https://3000-qjaogjiwnywohqixaomqcqiqkmfqdrqa.preview.same-app.com',
+    'https://3000-sayvgfzlkdtaeyacwzoctjevhesiqfie.preview.same-app.com',
+  ],
   webpack: (webpackConfig) => {
     webpackConfig.resolve.extensionAlias = {
       '.cjs': ['.cts', '.cjs'],
       '.js': ['.ts', '.tsx', '.js', '.jsx'],
       '.mjs': ['.mts', '.mjs'],
     }
-
     return webpackConfig
   },
 }
